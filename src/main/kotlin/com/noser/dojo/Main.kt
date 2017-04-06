bc3b469bf7bfff8ae842cd9e2eba4edfe347cb69
Fabian Mächler <fabian.maechler@noser.com>
2017-04-06 12:56:34 +0200

    Added RxKotlin

--- src/main/kotlin/com/noser/dojo/Main.kt
+++ src/main/kotlin/com/noser/dojo/Main.kt
@@ -1,7 +1,21 @@
 package com.noser.dojo
 
+import io.reactivex.rxkotlin.subscribeBy
+import io.reactivex.rxkotlin.toObservable
+
 object Main {
     @JvmStatic fun main(args: Array<String>) {
         println(Messager().getMessage())
+
+        val list = listOf("Alpha", "Beta", "Gamma", "Delta", "Epsilon")
+
+        list.toObservable()
+                .filter { it.length >= 5 }
+                .repeat(2)
+                .subscribeBy(
+                        onNext = ::println,
+                        onError = { it.printStackTrace() },
+                        onComplete = { println("Done!") }
+                )
     }
 }
