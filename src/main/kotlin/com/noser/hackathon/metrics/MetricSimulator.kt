3d013d3e3c6a394b38b06c813c32e5719d42b0f7
boris.ruch <borisruch@gmail.com>
2018-05-29 15:58:02 +0200

    update metric simulator

--- src/main/kotlin/com/noser/hackathon/metrics/MetricSimulator.kt
+++ src/main/kotlin/com/noser/hackathon/metrics/MetricSimulator.kt
@@ -6,6 +6,18 @@ import org.springframework.stereotype.Component
 @Component
 class MetricSimulator(val gameMetrics: Metrics) {
 
+
+    @Scheduled(fixedRate = 10000)
+    fun runMatch() {
+        gameMetrics.matchWon()
+        gameMetrics.matchLost()
+        if (Math.random() < 0.3) {
+            gameMetrics.matchLost()
+        } else {
+            gameMetrics.matchWon()
+        }
+    }
+
     @Scheduled(fixedRate = 1000)
     fun runGame() {
         gameMetrics.startMatch()
