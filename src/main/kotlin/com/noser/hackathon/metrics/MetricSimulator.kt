25c6388eddad959371c726466af9a1f4509a4d87
boris.ruch <borisruch@gmail.com>
2018-05-29 16:01:51 +0200

    update metric simulator

--- src/main/kotlin/com/noser/hackathon/metrics/MetricSimulator.kt
+++ src/main/kotlin/com/noser/hackathon/metrics/MetricSimulator.kt
@@ -20,10 +20,7 @@ class MetricSimulator(val gameMetrics: Metrics) {
 
     @Scheduled(fixedRate = 1000)
     fun runGame() {
-        gameMetrics.startMatch()
         gameMetrics.startGame()
-        gameMetrics.matchWon()
-        gameMetrics.matchLost()
         if (Math.random() < 0.3) {
             gameMetrics.gameLost()
         } else {
